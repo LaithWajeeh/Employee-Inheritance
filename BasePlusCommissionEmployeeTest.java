179311939d6c6f22b91dac0cad8b05f4f5e0f9db
lwajeeh <lwajeeh05@gmail.com>
2024-07-17 22:12:24 -0700

    More comments, more code, more grade.

--- BasePlusCommissionEmployeeTest.java
+++ BasePlusCommissionEmployeeTest.java
@@ -1,18 +1,39 @@
+// Name: Laith Wajeeh
+// Class: CS 145
+// Assignment: Lab_4: Payroll System Modification
+// Sources: w3schools.com and stackexchange.com.
+// Purpose: Demonstrates the use of polymorphism and inheritance to manage employee wages.
+
 public class BasePlusCommissionEmployeeTest {
     public static void main(String[] args) {
-        BasePlusCommissionEmployee emp = new BasePlusCommissionEmployee("Laith","Wajeeh","2024",500000, .10, 100000);
+        BasePlusCommissionEmployee emp = new BasePlusCommissionEmployee(
+            "Laith",
+            "Wajeeh",
+            "2024",
+            500000, 
+            .10, 
+            100000);
         
-        System.out.println(emp.getFirstName());
-        System.out.println(emp.getLastName());
-        System.out.println(emp.getSocialSecurityNumber());
-        System.out.println(emp.getFirstName());
-        System.out.println(emp.getFirstName());
+        System.out.println("Tests/Demos:\n\nFirst Name:" + emp.getFirstName());
+        System.out.println("Last Name:" + emp.getLastName());
+        System.out.println("Social Security Number:" + emp.getSocialSecurityNumber());
+        System.out.println("Gross Sales:" + emp.getGrossSales());
+        System.out.println("Commission Rate:" + emp.getCommissionRate());
+        System.out.println("Base Salary:" + emp.getBaseSalary() + "\n");
+        System.out.println("Earnings:" + emp.earnings());
+        System.err.println("\n------------------------------------\n\ntoString output:\n\n" + emp.toString());
+
+        System.err.println("\n------------------------------------\n\nChange some values:\n");
+        emp.setGrossSales(400000);
+        System.out.println("Gross Sales:" + emp.getGrossSales());
+        emp.setCommissionRate(0.20);
+        System.out.println("Commission Rate:" + emp.getCommissionRate());
+        emp.setBaseSalary(150000);
+        System.out.println("Base Salary:" + emp.getBaseSalary() + "\n");
+        System.out.println("Earnings:" + emp.earnings());
+        System.err.println("\n------------------------------------\n\ntoString output:\n\n" + emp.toString() + "\n");
 
 
-
-
-
-
-        System.out.println(emp.toString());
+        
     }
 }
